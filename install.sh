eb9cd333de9658eb6082a9cc8b0ac591d83f1788
Michiel van der Meer <michiel@dutchnaoteam.nl>
2018-02-06 15:45:29 +0100

    Added optional linking of i3 config

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ SCRIPT_PATH=$(echo $0 | sed "s|^\.\./|`pwd`/../|" | sed "s|^\./|`pwd`/|")
 DOT_FOLDER="$(dirname $SCRIPT_PATH)"
 echo "Installing dotfiles from folder $DOT_FOLDER"
 
+echo -n "Do you want to symlink general dotfiles like .bashrc, .gitconfig, etc.. ([y]/n): "
+read LINK_GENERAL
+LINK_GENERAL=${LINK_GENERAL:-y}
+echo -n "Do you want to symlink i3 config ([y]/n): "
+read LINK_i3
+LINK_i3=${LINK_i3:-y}
+
 if [ -z ${HOME+x} ]; then
     echo "HOME is unset, not able to check for existing links";
     exit 1
@@ -21,54 +28,60 @@ fi
 export PROJECT_FOLDER="$(dirname $DOT_FOLDER)"
 echo "PROJECT_FOLDER is set to '$PROJECT_FOLDER'"
 
-echo "Setting symlinks.."
-SYS_FILES="$DOT_FOLDER/system/.??*"
-GIT_FILES="$DOT_FOLDER/git/.??*"
-VIM_FILES="$DOT_FOLDER/vim/.??*"
-for f in $SYS_FILES $GIT_FILES $VIM_FILES
-do
+if [[ "$LINK_GENERAL" == "y" ]]; then
+  echo "Setting general symlinks.."
+  SYS_FILES="$DOT_FOLDER/system/.??*"
+  GIT_FILES="$DOT_FOLDER/git/.??*"
+  VIM_FILES="$DOT_FOLDER/vim/.??*"
+  for f in $SYS_FILES $GIT_FILES $VIM_FILES
+  do
     file="$(basename $f)"
     if [[ ! -e  "$HOME/$file" ]]; then
-        echo "Linking $file to $HOME/"
-        ln -s $f "$HOME/$file"
+      echo "Linking $file to $HOME/"
+      ln -s $f "$HOME/$file"
     else
-        echo "$file already linked"
+      echo "$file already linked"
     fi
-done
-# Link i3 files to ~/.config/i3/
-i3_FILES="$DOT_FOLDER/i3/??*"
-i3_CONFIG_DIR="$HOME/.config/i3/"
-# check if i3 config directory exists
-if [[ ! -d $i3_CONFIG_DIR ]]; then
-  mkdir -p $i3_CONFIG_DIR
+  done
 fi
-for f in $i3_FILES
-do
-  file="$(basename $f)"
-  if [[ ! -e  "$i3_CONFIG_DIR/$file" ]]; then
+
+if [[ "$LINK_i3" == "y" ]]; then
+  echo "Linking i3 related configuration.."
+  # Link i3 files to ~/.config/i3/
+  i3_FILES="$DOT_FOLDER/i3/??*"
+  i3_CONFIG_DIR="$HOME/.config/i3/"
+  # check if i3 config directory exists
+  if [[ ! -d $i3_CONFIG_DIR ]]; then
+    mkdir -p $i3_CONFIG_DIR
+  fi
+  for f in $i3_FILES
+  do
+    file="$(basename $f)"
+    if [[ ! -e  "$i3_CONFIG_DIR/$file" ]]; then
       echo "Linking $file to $i3_CONFIG_DIR"
       ln -s $f "$i3_CONFIG_DIR/$file"
-  else
-      echo "$file already linked"
-  fi
-done
+    else
+      echo "$i3_CONFIG_DIR/$file already linked"
+    fi
+  done
 
-i3_STATUS_FILES="$DOT_FOLDER/i3status/??*"
-i3_STATUS_CONFIG_DIR="$HOME/.config/i3status/"
-# check if i3status config directory exists
-if [[ ! -d $i3_STATUS_CONFIG_DIR ]]; then
-  mkdir -p $i3_STATUS_CONFIG_DIR
-fi
-for f in $i3_STATUS_FILES
-do
-  file="$(basename $f)"
-  if [[ ! -e  "$i3_STATUS_CONFIG_DIR/$file" ]]; then
+  i3_STATUS_FILES="$DOT_FOLDER/i3status/??*"
+  i3_STATUS_CONFIG_DIR="$HOME/.config/i3status/"
+  # check if i3status config directory exists
+  if [[ ! -d $i3_STATUS_CONFIG_DIR ]]; then
+    mkdir -p $i3_STATUS_CONFIG_DIR
+  fi
+  for f in $i3_STATUS_FILES
+  do
+    file="$(basename $f)"
+    if [[ ! -e  "$i3_STATUS_CONFIG_DIR/$file" ]]; then
       echo "Linking $file to $i3_STATUS_CONFIG_DIR/"
       ln -s $f "$i3_STATUS_CONFIG_DIR/$file"
-  else
-      echo "$file already linked"
-  fi
-done
+    else
+      echo "$i3_STATUS_CONFIG_DIR/$file already linked"
+    fi
+  done
+fi
 
 # Install greeter
 sudo apt install fortune cowsay
