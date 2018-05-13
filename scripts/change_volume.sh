cbe1ec6c7b19d965ab0cfa0804c9fc2000393eb9
Michiel van der Meer <michiel@dutchnaoteam.nl>
2018-05-13 16:43:38 +0200

    Fixed volume control, new color in i3bar, gitconf shortcut

--- scripts/change_volume.sh
+++ scripts/change_volume.sh
@@ -4,15 +4,15 @@
 case "$1" in
 
   up)
-    pactl set-sink-volume 0 +5%
+    pactl set-sink-volume 1 +5%
     paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
     ;;
   down)
-    pactl set-sink-volume 0 -5%
+    pactl set-sink-volume 1 -5%
     paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
     ;;
   toggle)
-    pactl set-sink-mute 0 toggle
+    pactl set-sink-mute 1 toggle
     ;;
   *)
     echo -ne "Usage : change_volume <up|down|mute>\n"
